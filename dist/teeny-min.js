45ebd44199a1886a0d218d0e63adb94b0e065489
dlgulley411@gmail.com <dlgulley411@gmail.com>
2015-07-28 12:15:56 -0400

    initial commit

---
+++ dist/teeny-min.js
@@ -0,0 +1,7 @@
+'use strict';(function(){var cache=Object.create(Object);window.teenyjs={};function buildModule(name){if(cache[name].data!==undefined){return;}
+var args=[];cache[name].deps.forEach(function(dep){if(cache[dep].data===undefined){buildModule(dep);}
+args.push(cache[dep].data);});cache[name].data=cache[name].moduleFunction.apply(undefined,args);};function areDependenciesLoaded(deps){for(var i=0;i<deps.length;++i){if(cache.hasOwnProperty(deps[i])===false||cache[deps[i]].data===undefined){return false;}}
+return true;};window.teenyjs.require=function(names,scopedFunction){var args=[];names.forEach(function(name){if(cache.hasOwnProperty(name)){if(cache[name].data===undefined){buildModule(name);}
+args.push(cache[name].data);}});scopedFunction.apply(undefined,args);};window.teenyjs.define=function(name,deps,moduleFunction){if(cache.hasOwnProperty(name)){throw'module '+name+' is already defined';}
+cache[name]={deps:deps,moduleFunction:moduleFunction,data:undefined}
+if(deps.length===0){setTimeout(function(){buildModule(name);});}else{if(areDependenciesLoaded(deps)===true){setTimeout(function(){buildModule(name);});}}};})();
---
+++ source/teeny.js
@@ -0,0 +1,150 @@
+'use strict';
+( function () {
+
+	//Modules as properties
+	var cache = Object.create( Object );
+
+	//Global reference to our module
+	window.teenyjs = {};
+
+	/**
+	 * Resolves a module and it's dependencies
+	 * @param  {[type]} name [description]
+	 * @return {[type]}      [description]
+	 */
+	function buildModule( name ) {
+
+		/**
+		 * Since we begin loading from the event queue it's possible to hit this more than once, just exit if you fo
+		 * @param  {[type]} cache[name].data !             [description]
+		 * @return {[type]}                  [description]
+		 */
+		if ( cache[name].data !== undefined ) {
+
+			return;
+
+		}
+
+		var args = [];
+
+		cache[name].deps.forEach( function ( dep ) {
+
+			if ( cache[dep].data === undefined ) {
+
+				buildModule( dep );
+
+			}
+
+			args.push( cache[dep].data );
+
+		});
+
+		cache[name].data = cache[name].moduleFunction.apply( undefined, args );
+
+	};
+
+	/**
+	 * Checks if the dependencies are loaded
+	 * @param  {[type]} deps [description]
+	 * @return {[type]}      [description]
+	 */
+	function areDependenciesLoaded ( deps ) {
+
+		for ( var i = 0; i < deps.length; ++i ) {
+
+			//This is blowing the stack by recursively checking data on load, for now we'll just make sure any
+			//modules we load from the get go have all dependecies completely loaded
+			if ( cache.hasOwnProperty( deps[i] ) === false || cache[deps[i]].data === undefined ) {
+
+				return false;
+
+			}
+
+		}
+
+		return true;
+
+	};
+
+	/**
+	 * Sets the module and applies it to the supplied function
+	 * @param  {[type]} names          [description]
+	 * @param  {[type]} scopedFunction [description]
+	 * @return {[type]}                [description]
+	 */
+	window.teenyjs.require = function ( names, scopedFunction ) {
+
+		var args = [];
+
+		names.forEach( function ( name ) {
+
+			if ( cache.hasOwnProperty( name ) ) {
+
+				//If this module hasnt been set, set it now
+				if ( cache[name].data === undefined ) {
+
+
+					buildModule( name );
+
+				}
+
+				args.push( cache[name].data );
+
+			}
+
+		});
+
+		//Execute the scoped function with the provided dependencies
+		scopedFunction.apply( undefined, args );
+
+	};
+
+	/**
+	 * Gets the function to create the module ready
+	 * @param  {[type]} name           [description]
+	 * @param  {[type]} moduleFunction [description]
+	 * @return {[type]}                [description]
+	 */
+	window.teenyjs.define = function ( name, deps, moduleFunction ) {
+
+		if ( cache.hasOwnProperty( name ) ) {
+
+			throw 'module ' + name + ' is already defined';
+
+		}
+
+		cache[name] = {
+
+			deps: deps,
+			moduleFunction: moduleFunction,
+			data: undefined
+
+		}
+
+		//If there are no hindrances throw this on the event queue to build
+		if ( deps.length === 0 ) {
+
+			setTimeout( function () {
+
+				buildModule( name );
+
+			});
+
+		} else {
+
+			if ( areDependenciesLoaded( deps ) === true ) {
+
+				//If were here all dependecies are already loaded, throw this on the event queue to build
+				setTimeout( function () {
+
+					buildModule( name );
+
+				});
+
+			}
+
+		}
+
+	};
+
+})();
